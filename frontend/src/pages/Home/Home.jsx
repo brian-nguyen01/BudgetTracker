8ec81841ea9db361ef26568a81e188fdfb911122
brian-nguyen01 <briannguyen980@gmail.com>
2024-05-21 22:57:51 -0700

    set theme colors

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
-
+import './Home.scss';
 const Home = () => {
   return (
     <div>
-      <h1>Home Page</h1>
+        <h1 className='a'>Color Test</h1>
+        <h1 className='b'>Color Test</h1>
+        <div className='dark'>
+            <h1 className='c'>Color Test</h1>
+            <h1 className='d'>Color Test</h1>
+        </div>
+
+
     </div>
   );
 }
