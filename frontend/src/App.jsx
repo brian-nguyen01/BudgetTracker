f9df6e16b353264eb49b2fb7727a0ba27258abc5
brian-nguyen01 <briannguyen980@gmail.com>
2024-05-21 22:07:32 -0700

    theme+fonts+pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,23 @@
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+const Home = lazy(() => import('./pages/Home/Home'));
+const About = lazy(() => import('./pages/About/About'));
+const Contact = lazy(() => import('./pages/Contact/Contact'));
 
 const App = () => {
   return (
-    <div>
-    </div>
+    <Router>
+      <div>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Suspense>
+      </div>
+    </Router>
   );
 }
 
